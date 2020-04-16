4ebb961223402feba396f2581f16ec6c2460b0fd
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-16 10:18:42 -0600

    finished work on weds

--- Step2_ARFIMA_v0-1.R
+++ Step2_ARFIMA_v0-1.R
@@ -428,7 +428,7 @@ c(fracdiff.AICC(fit.microsoft_2.1d0),fracdiff.AIC(fit.microsoft_2.1d0),fracdiff.
 c(fracdiff.AICC(fit.microsoft_2.0d1),fracdiff.AIC(fit.microsoft_2.0d1),fracdiff.BIC(fit.microsoft_2.0d1))
 c(fracdiff.AICC(fit.microsoft_2.1d1),fracdiff.AIC(fit.microsoft_2.1d1),fracdiff.BIC(fit.microsoft_2.1d1))
 
-### microsoft_1 model diagnostics: autocorrelation in residuals
+### microsoft_2 model diagnostics: autocorrelation in residuals
 fit.microsoft_2.bst <- fit.microsoft_2.0d0                                                       # this minimizes AICC, AIC, BIC
 
 r.t_microsoft_2 <- fit.microsoft_2.bst$residuals
@@ -443,7 +443,7 @@ abline(h=0,col="blue",lty=2)
 acf(r.t_microsoft_2,lag.max=100,ylim=c(-0.2,1),main="")
 pacf(r.t_microsoft_2,lag.max=100,ylim=c(-0.2,1),main="")
 
-### microsoft_1 residual normality check
+### microsoft_2 residual normality check
 dev.new(height=6,width=12)
 par(mfrow=c(1,2),mex=0.75)
 hist(r.t_microsoft_2,                                                                           # histogram of residuals
@@ -460,3 +460,136 @@ shapiro.test(r.t_microsoft_2)                                                   
 ks.test(r.t_microsoft_2,"pnorm",mean=mean(r.t_microsoft_2),sd=sd(r.t_microsoft_2))              # Kolmogorov-Smirnov indicates normality
 
 
+
+
+### oracle_1 ARFIMA model
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(v.t_oracle_1,ylim=c(0,3),
+        xlab="Year",ylab="GK volatility",main="Oracle Volatility 1/01/2016-12/31/2017")
+acf(v.t_oracle_1,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(v.t_oracle_1,lag.max=100,ylim=c(-0.2,1),main="")
+
+fit.oracle_1.0d0 <- fracdiff(v.t_oracle_1-mean(v.t_oracle_1),nar=0,nma=0,M=50)                  # d term significant  
+summary(fit.oracle_1.0d0)
+
+fit.oracle_1.1d0 <- fracdiff(v.t_oracle_1-mean(v.t_oracle_1),nar=1,nma=0,M=50)                  # ar, d term significant
+summary(fit.oracle_1.1d0)
+
+fit.oracle_1.2d0 <- fracdiff(v.t_oracle_1-mean(v.t_oracle_1),nar=2,nma=0,M=50)                  # only d, ar1 term significant
+summary(fit.oracle_1.2d0)
+
+fit.oracle_1.0d1 <- fracdiff(v.t_oracle_1-mean(v.t_oracle_1),nar=0,nma=1,M=50)                  # ma term significant
+summary(fit.oracle_1.0d1)
+
+fit.oracle_1.0d2 <- fracdiff(v.t_oracle_1-mean(v.t_oracle_1),nar=0,nma=2,M=50)                  # ma1 term significant
+summary(fit.oracle_1.0d2)
+
+fit.oracle_1.1d1 <- fracdiff(v.t_oracle_1-mean(v.t_oracle_1),nar=1,nma=1,M=50)                  # only d term significant          
+summary(fit.oracle_1.1d1)
+
+fit.oracle_1.0d0_b <- fracdiff(v.t_oracle_1-mean(v.t_oracle_1),nar=0,nma=0,M=30)                # changed M value to 30, results are the same as above
+summary(fit.oracle_1.0d0_b)
+
+
+c(fracdiff.AICC(fit.oracle_1.0d0),fracdiff.AIC(fit.oracle_1.0d0),fracdiff.BIC(fit.oracle_1.0d0))  
+c(fracdiff.AICC(fit.oracle_1.1d0),fracdiff.AIC(fit.oracle_1.1d0),fracdiff.BIC(fit.oracle_1.1d0))
+c(fracdiff.AICC(fit.oracle_1.0d1),fracdiff.AIC(fit.oracle_1.0d1),fracdiff.BIC(fit.oracle_1.0d1))
+# [Q] the three models above are very close -- if this is the case, which criterion is used?
+
+
+
+### oracle_1 model diagnostics: autocorrelation in residuals
+fit.oracle_1.bst <- fit.oracle_1.0d0                                                          # this minimizes BIC but AICC and AIC are very close to min val
+
+r.t_oracle_1 <- fit.oracle_1.bst$residuals
+summary(r.t_oracle_1)                                                                         
+
+
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(r.t_oracle_1,ylim=c(-2,2),
+        xlab="Year",ylab="GK volatility",main="Oracle Volatility Residuals 1/01/2016-12/31/2017")
+abline(h=0,col="blue",lty=2)
+acf(r.t_oracle_1,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(r.t_oracle_1,lag.max=100,ylim=c(-0.2,1),main="")
+
+### oracle_1 residual normality check
+dev.new(height=6,width=12)
+par(mfrow=c(1,2),mex=0.75)
+hist(r.t_oracle_1,                                                                            # histogram of residuals
+     breaks=seq(-2,2,0.25),
+     freq=FALSE,
+     col="grey85",ylim=c(0,3),
+     main="Residual Histogram")                                                              
+z <- seq(-60,60,length=1000)                                      
+lines(z,dnorm(z,mean=mean(r.t_oracle_1),sd=sd(r.t_oracle_1)),lty=1,col="red")                # add theoretical normal density
+qqnorm(r.t_oracle_1)                                                                         # [Q] Does this QQ plot support normality? Hard to tell at the ends
+
+shapiro.test(r.t_oracle_1)                                                                   # Shapiro-Wilk normality test supports normality
+ks.test(r.t_oracle_1,"pnorm",mean=mean(r.t_oracle_1),sd=sd(r.t_oracle_1))                    # KS test supports normality
+
+
+### oracle_2 ARFIMA model
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(v.t_oracle_2,ylim=c(0,3),
+        xlab="Year",ylab="GK volatility",main="Oracle Volatility 2/01/2018-12/31/2019")
+acf(v.t_oracle_2,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(v.t_oracle_2,lag.max=100,ylim=c(-0.2,1),main="")
+
+fit.oracle_2.0d0 <- fracdiff(v.t_oracle_2-mean(v.t_oracle_2),nar=0,nma=0,M=50)                # d term significant  
+summary(fit.oracle_2.0d0)
+
+fit.oracle_2.1d0 <- fracdiff(v.t_oracle_2-mean(v.t_oracle_2),nar=1,nma=0,M=50)                # ar term not significant
+summary(fit.oracle_2.1d0)
+
+fit.oracle_2.0d1 <- fracdiff(v.t_oracle_2-mean(v.t_oracle_2),nar=0,nma=1,M=50)                # ma term almost significant (p=0.0633)
+summary(fit.oracle_2.0d1)
+
+fit.oracle_2.0d2 <- fracdiff(v.t_oracle_2-mean(v.t_oracle_2),nar=0,nma=2,M=50)                # ma1 term significant
+summary(fit.oracle_2.0d2)
+
+fit.oracle_2.1d1 <- fracdiff(v.t_oracle_2-mean(v.t_oracle_2),nar=1,nma=1,M=50)                # all three terms significant      
+summary(fit.oracle_2.1d1)
+
+fit.oracle_2.1d2 <- fracdiff(v.t_oracle_2-mean(v.t_oracle_2),nar=1,nma=2,M=50)                # warning when calculating correlation matrix    
+summary(fit.oracle_2.1d2)
+
+
+c(fracdiff.AICC(fit.oracle_2.0d0),fracdiff.AIC(fit.oracle_2.0d0),fracdiff.BIC(fit.oracle_2.0d0))  
+c(fracdiff.AICC(fit.oracle_2.1d1),fracdiff.AIC(fit.oracle_2.1d1),fracdiff.BIC(fit.oracle_2.1d1))
+c(fracdiff.AICC(fit.oracle_2.0d1),fracdiff.AIC(fit.oracle_2.0d1),fracdiff.BIC(fit.oracle_2.0d1))
+# [Q] the three models above are very close -- if this is the case, which criterion is used? 
+
+
+
+### oracle_2 model diagnostics: autocorrelation in residuals
+fit.oracle_2.bst <- fit.oracle_2.0d0                                                          # this minimizes BIC but AICC and AIC are very close to min val
+
+r.t_oracle_2 <- fit.oracle_2.bst$residuals
+summary(r.t_oracle_2)                                                                         
+
+
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(r.t_oracle_2,ylim=c(-2,2),
+        xlab="Year",ylab="GK volatility",main="Oracle Volatility Residuals 2/01/2018-12/31/2019")
+abline(h=0,col="blue",lty=2)
+acf(r.t_oracle_2,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(r.t_oracle_2,lag.max=100,ylim=c(-0.2,1),main="")
+
+### oracle_2 residual normality check
+dev.new(height=6,width=12)
+par(mfrow=c(1,2),mex=0.75)
+hist(r.t_oracle_2,                                                                           # histogram of residuals
+     breaks=seq(-2,2,0.25),
+     freq=FALSE,
+     col="grey85",ylim=c(0,3),
+     main="Residual Histogram")                                                              
+z <- seq(-60,60,length=1000)                                      
+lines(z,dnorm(z,mean=mean(r.t_oracle_2),sd=sd(r.t_oracle_2)),lty=1,col="red")                # add theoretical normal density
+qqnorm(r.t_oracle_2)                                                                         # [Q] Does this QQ plot support normality? Hard to tell at the ends
+
+shapiro.test(r.t_oracle_2)                                                                   # Shapiro-Wilk normality test supports normality
+ks.test(r.t_oracle_2,"pnorm",mean=mean(r.t_oracle_2),sd=sd(r.t_oracle_2))                    # KS test supports normality
