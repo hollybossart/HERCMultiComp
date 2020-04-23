26db429bf897bec8b8b756eca1a917a5d567bb39
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-23 11:42:32 -0600

    bankofa1 added

--- Step2_ARFIMA_v0-2.R
+++ Step2_ARFIMA_v0-2.R
@@ -1599,6 +1599,79 @@ ks.test(r.t_intel_1,"pnorm",mean=mean(r.t_intel_1),sd=sd(r.t_intel_1))          
 bst.models[nrow(bst.models)+1,] <- c("Intel", 1, 0, fit.intel_1.bst$d, 0)     
 
 
+### bankofa_1 ARFIMA model
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(v.t_bankofa_1,ylim=c(0,1),                                                             
+        xlab="Year",ylab="GK volatility",main="Bank of America Volatility 1/01/2016-12/31/2017")
+acf(v.t_bankofa_1,lag.max=100,ylim=c(-0.2,1),main="")                                                  
+pacf(v.t_bankofa_1,lag.max=100,ylim=c(-0.2,1),main="")                                        
+
+fit.bankofa_1.0d0 <- fracdiff(v.t_bankofa_1-mean(v.t_bankofa_1),nar=0,nma=0,M=50)                        # d term significant  
+summary(fit.bankofa_1.0d0)
+
+fit.bankofa_1.1d0 <- fracdiff(v.t_bankofa_1-mean(v.t_bankofa_1),nar=1,nma=0,M=50)                        # ar term not sig
+summary(fit.bankofa_1.1d0)
+
+fit.bankofa_1.2d0 <- fracdiff(v.t_bankofa_1-mean(v.t_bankofa_1),nar=2,nma=0,M=50)                        # ar terms not sig
+summary(fit.bankofa_1.2d0)
+
+fit.bankofa_1.0d1 <- fracdiff(v.t_bankofa_1-mean(v.t_bankofa_1),nar=0,nma=1,M=50)                        # ma not sig
+summary(fit.bankofa_1.0d1)
+
+fit.bankofa_1.0d2 <- fracdiff(v.t_bankofa_1-mean(v.t_bankofa_1),nar=0,nma=2,M=50)                        # ma terms not sig
+summary(fit.bankofa_1.0d2)
+
+fit.bankofa_1.1d1 <- fracdiff(v.t_bankofa_1-mean(v.t_bankofa_1),nar=1,nma=1,M=50)                        # all sig
+summary(fit.bankofa_1.1d1)
+
+fit.bankofa_1.1d2 <- fracdiff(v.t_bankofa_1-mean(v.t_bankofa_1),nar=1,nma=2,M=50)                        # ma 2 not sig
+summary(fit.bankofa_1.1d2)
+
+fit.bankofa_1.2d1 <- fracdiff(v.t_bankofa_1-mean(v.t_bankofa_1),nar=2,nma=1,M=50)                        # ar2 not sig   
+summary(fit.bankofa_1.2d1)
+
+
+
+c(fracdiff.AICC(fit.bankofa_1.0d0),fracdiff.AIC(fit.bankofa_1.0d0),fracdiff.BIC(fit.bankofa_1.0d0))
+c(fracdiff.AICC(fit.bankofa_1.1d1),fracdiff.AIC(fit.bankofa_1.1d1),fracdiff.BIC(fit.bankofa_1.1d1)) 
+
+
+
+### bankofa_1 model diagnostics: autocorrelation in residuals
+fit.bankofa_1.bst <- fit.bankofa_1.0d0                                                         
+
+r.t_bankofa_1 <- fit.bankofa_1.bst$residuals
+summary(r.t_bankofa_1)                                                                         
+
+
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(r.t_bankofa_1,ylim=c(-0.5,0.5),
+        xlab="Year",ylab="GK volatility",main="Bank of America Volatility Residuals 1/01/2016-12/31/2017")
+abline(h=0,col="blue",lty=2)
+acf(r.t_bankofa_1,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(r.t_bankofa_1,lag.max=100,ylim=c(-0.2,1),main="")
+
+### bankofa_1 residual normality check
+dev.new(height=6,width=12)
+par(mfrow=c(1,2),mex=0.75)
+hist(r.t_bankofa_1,                                                                                   
+     breaks=seq(-1,1,0.125),
+     freq=FALSE,
+     col="grey85",ylim=c(0,5),
+     main="Residual Histogram")                                                              
+z <- seq(-60,60,length=1000)                                      
+lines(z,dnorm(z,mean=mean(r.t_bankofa_1),sd=sd(r.t_bankofa_1)),lty=1,col="red")               
+qqnorm(r.t_bankofa_1)                                                                         
+qqline(r.t_bankofa_1)
+
+shapiro.test(r.t_bankofa_1)                                                                            # Shapiro-Wilk normality test supports normality
+ks.test(r.t_bankofa_1,"pnorm",mean=mean(r.t_bankofa_1),sd=sd(r.t_bankofa_1))                           # KS test supports normality
+
+bst.models[nrow(bst.models)+1,] <- c("Bank of America", 1, 0, fit.bankofa_1.bst$d, 0)     
+
+
 
 ### MODEL FITTING PART TWO DATA ---------------------------------------------------------------------
 
