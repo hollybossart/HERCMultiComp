c6a8f7c0a0cfc2f8f8a8b0a46dbf7bab1ae7cf24
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-23 11:27:25 -0600

    walmart 1 added

--- Step2_ARFIMA_v0-2.R
+++ Step2_ARFIMA_v0-2.R
@@ -1451,6 +1451,80 @@ bst.models[nrow(bst.models)+1,] <- c("JP Morgan", 1, 0, fit.jpmorgan_1.bst$d, 0)
 
 
 
+### walmart_1 ARFIMA model
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(v.t_walmart_1,ylim=c(0,3),                                                             
+        xlab="Year",ylab="GK volatility",main="Walmart Volatility 1/01/2016-12/31/2017")
+acf(v.t_walmart_1,lag.max=100,ylim=c(-0.2,1),main="")                                                  
+pacf(v.t_walmart_1,lag.max=100,ylim=c(-0.2,1),main="")                                        
+
+fit.walmart_1.0d0 <- fracdiff(v.t_walmart_1-mean(v.t_walmart_1),nar=0,nma=0,M=50)                        # d term significant  
+summary(fit.walmart_1.0d0)
+
+fit.walmart_1.1d0 <- fracdiff(v.t_walmart_1-mean(v.t_walmart_1),nar=1,nma=0,M=50)                        # ar term not significant
+summary(fit.walmart_1.1d0)
+
+fit.walmart_1.2d0 <- fracdiff(v.t_walmart_1-mean(v.t_walmart_1),nar=2,nma=0,M=50)                        # ar terms not sig
+summary(fit.walmart_1.2d0)
+
+fit.walmart_1.0d1 <- fracdiff(v.t_walmart_1-mean(v.t_walmart_1),nar=0,nma=1,M=50)                        # ma not sig
+summary(fit.walmart_1.0d1)
+
+fit.walmart_1.0d2 <- fracdiff(v.t_walmart_1-mean(v.t_walmart_1),nar=0,nma=2,M=50)                       # only d sig
+summary(fit.walmart_1.0d2)
+
+fit.walmart_1.1d1 <- fracdiff(v.t_walmart_1-mean(v.t_walmart_1),nar=1,nma=1,M=50)                       # all sig
+summary(fit.walmart_1.1d1)
+
+fit.walmart_1.1d2 <- fracdiff(v.t_walmart_1-mean(v.t_walmart_1),nar=1,nma=2,M=50)                       # warning
+summary(fit.walmart_1.1d2)
+
+fit.walmart_1.2d1 <- fracdiff(v.t_walmart_1-mean(v.t_walmart_1),nar=2,nma=1,M=50)                       # all sig  
+summary(fit.walmart_1.2d1)
+
+
+
+c(fracdiff.AICC(fit.walmart_1.0d0),fracdiff.AIC(fit.walmart_1.0d0),fracdiff.BIC(fit.walmart_1.0d0))
+c(fracdiff.AICC(fit.walmart_1.1d1),fracdiff.AIC(fit.walmart_1.1d1),fracdiff.BIC(fit.walmart_1.1d1)) 
+c(fracdiff.AICC(fit.walmart_1.2d1),fracdiff.AIC(fit.walmart_1.2d1),fracdiff.BIC(fit.walmart_1.2d1))
+
+
+
+### walmart_1 model diagnostics: autocorrelation in residuals
+fit.walmart_1.bst <- fit.walmart_1.0d0                                                         
+
+r.t_walmart_1 <- fit.walmart_1.bst$residuals
+summary(r.t_walmart_1)                                                                         
+
+
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(r.t_walmart_1,ylim=c(-1,1.5),
+        xlab="Year",ylab="GK volatility",main="Walmart Volatility Residuals 1/01/2016-12/31/2017")
+abline(h=0,col="blue",lty=2)
+acf(r.t_walmart_1,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(r.t_walmart_1,lag.max=100,ylim=c(-0.2,1),main="")
+
+### walmart_1 residual normality check
+dev.new(height=6,width=12)
+par(mfrow=c(1,2),mex=0.75)
+hist(r.t_walmart_1,                                                                                   
+     breaks=seq(-2,2,0.25),
+     freq=FALSE,
+     col="grey85",ylim=c(0,3),
+     main="Residual Histogram")                                                              
+z <- seq(-60,60,length=1000)                                      
+lines(z,dnorm(z,mean=mean(r.t_walmart_1),sd=sd(r.t_walmart_1)),lty=1,col="red")               
+qqnorm(r.t_walmart_1)                                                                         
+qqline(r.t_walmart_1)
+
+shapiro.test(r.t_walmart_1)                                                                            # Shapiro-Wilk normality test supports normality
+ks.test(r.t_walmart_1,"pnorm",mean=mean(r.t_walmart_1),sd=sd(r.t_walmart_1))                           # KS test supports normality
+
+bst.models[nrow(bst.models)+1,] <- c("Walmart", 1, 0, fit.walmart_1.bst$d, 0)     
+
+
 ### MODEL FITTING PART TWO DATA ---------------------------------------------------------------------
 
 ### microsoft_2 ARFIMA model
