bb2fe13b7985b903929adf5597c897862c015c01
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-23 11:32:41 -0600

    intel 1 added

--- Step2_ARFIMA_v0-2.R
+++ Step2_ARFIMA_v0-2.R
@@ -1525,6 +1525,81 @@ ks.test(r.t_walmart_1,"pnorm",mean=mean(r.t_walmart_1),sd=sd(r.t_walmart_1))    
 bst.models[nrow(bst.models)+1,] <- c("Walmart", 1, 0, fit.walmart_1.bst$d, 0)     
 
 
+
+### intel_1 ARFIMA model
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(v.t_intel_1,ylim=c(0,1.5),                                                             
+        xlab="Year",ylab="GK volatility",main="Intel Volatility 1/01/2016-12/31/2017")
+acf(v.t_intel_1,lag.max=100,ylim=c(-0.2,1),main="")                                                  
+pacf(v.t_intel_1,lag.max=100,ylim=c(-0.2,1),main="")                                        
+
+fit.intel_1.0d0 <- fracdiff(v.t_intel_1-mean(v.t_intel_1),nar=0,nma=0,M=50)                        # d term significant  
+summary(fit.intel_1.0d0)
+
+fit.intel_1.1d0 <- fracdiff(v.t_intel_1-mean(v.t_intel_1),nar=1,nma=0,M=50)                        # ar term not significant
+summary(fit.intel_1.1d0)
+
+fit.intel_1.2d0 <- fracdiff(v.t_intel_1-mean(v.t_intel_1),nar=2,nma=0,M=50)                        # ar terms not sig
+summary(fit.intel_1.2d0)
+
+fit.intel_1.0d1 <- fracdiff(v.t_intel_1-mean(v.t_intel_1),nar=0,nma=1,M=50)                        # ma not sig
+summary(fit.intel_1.0d1)
+
+fit.intel_1.0d2 <- fracdiff(v.t_intel_1-mean(v.t_intel_1),nar=0,nma=2,M=50)                        # only d sig
+summary(fit.intel_1.0d2)
+
+fit.intel_1.1d1 <- fracdiff(v.t_intel_1-mean(v.t_intel_1),nar=1,nma=1,M=50)                        # all sig
+summary(fit.intel_1.1d1)
+
+fit.intel_1.1d2 <- fracdiff(v.t_intel_1-mean(v.t_intel_1),nar=1,nma=2,M=50)                        # ma2 not sig
+summary(fit.intel_1.1d2)
+
+fit.intel_1.2d1 <- fracdiff(v.t_intel_1-mean(v.t_intel_1),nar=2,nma=1,M=50)                        # warning   
+summary(fit.intel_1.2d1)
+
+
+
+c(fracdiff.AICC(fit.intel_1.0d0),fracdiff.AIC(fit.intel_1.0d0),fracdiff.BIC(fit.intel_1.0d0))
+c(fracdiff.AICC(fit.intel_1.1d1),fracdiff.AIC(fit.intel_1.1d1),fracdiff.BIC(fit.intel_1.1d1)) 
+
+
+
+### intel_1 model diagnostics: autocorrelation in residuals
+fit.intel_1.bst <- fit.intel_1.0d0                                                         
+
+r.t_intel_1 <- fit.intel_1.bst$residuals
+summary(r.t_intel_1)                                                                         
+
+
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(r.t_intel_1,ylim=c(-1,1),
+        xlab="Year",ylab="GK volatility",main="Intel Volatility Residuals 1/01/2016-12/31/2017")
+abline(h=0,col="blue",lty=2)
+acf(r.t_intel_1,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(r.t_intel_1,lag.max=100,ylim=c(-0.2,1),main="")
+
+### intel_1 residual normality check
+dev.new(height=6,width=12)
+par(mfrow=c(1,2),mex=0.75)
+hist(r.t_intel_1,                                                                                   
+     breaks=seq(-2,2,0.25),
+     freq=FALSE,
+     col="grey85",ylim=c(0,3),
+     main="Residual Histogram")                                                              
+z <- seq(-60,60,length=1000)                                      
+lines(z,dnorm(z,mean=mean(r.t_intel_1),sd=sd(r.t_intel_1)),lty=1,col="red")               
+qqnorm(r.t_intel_1)                                                                         
+qqline(r.t_intel_1)
+
+shapiro.test(r.t_intel_1)                                                                            # Shapiro-Wilk normality test supports normality
+ks.test(r.t_intel_1,"pnorm",mean=mean(r.t_intel_1),sd=sd(r.t_intel_1))                               # KS test supports normality
+
+bst.models[nrow(bst.models)+1,] <- c("Intel", 1, 0, fit.intel_1.bst$d, 0)     
+
+
+
 ### MODEL FITTING PART TWO DATA ---------------------------------------------------------------------
 
 ### microsoft_2 ARFIMA model
