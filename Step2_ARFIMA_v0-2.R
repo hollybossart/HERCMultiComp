29e14d21303b4317c6168b9ebf3810afc40ad799
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-22 23:36:15 -0600

    chevron 1 done

--- Step2_ARFIMA_v0-2.R
+++ Step2_ARFIMA_v0-2.R
@@ -759,7 +759,7 @@ summary(r.t_facebook_1)
 
 dev.new(width=12,height=6)
 par(mfrow=c(3,1),mex=0.75)
-plot.ts(r.t_facebook_1,ylim=c(-2,2),
+plot.ts(r.t_facebook_1,ylim=c(-2,5),
         xlab="Year",ylab="GK volatility",main="Facebook Volatility Residuals 1/01/2016-12/31/2017")
 abline(h=0,col="blue",lty=2)
 acf(r.t_facebook_1,lag.max=100,ylim=c(-0.2,1),main="")
@@ -768,22 +768,95 @@ pacf(r.t_facebook_1,lag.max=100,ylim=c(-0.2,1),main="")
 ### facebook_1 residual normality check
 dev.new(height=6,width=12)
 par(mfrow=c(1,2),mex=0.75)
-hist(r.t_facebook_1,                                                                           # one outlier
+hist(r.t_facebook_1,                                                                                # [Q] some outliers?
      breaks=seq(-2,5,0.25),
      freq=FALSE,
      col="grey85",ylim=c(0,3),
      main="Residual Histogram")                                                              
 z <- seq(-60,60,length=1000)                                      
-lines(z,dnorm(z,mean=mean(r.t_facebook_1),sd=sd(r.t_facebook_1)),lty=1,col="red")                   # add theoretical normal density
+lines(z,dnorm(z,mean=mean(r.t_facebook_1),sd=sd(r.t_facebook_1)),lty=1,col="red")               
 qqnorm(r.t_facebook_1)                                                                         
 qqline(r.t_facebook_1)
 
-shapiro.test(r.t_facebook_1)                                                                   # Shapiro-Wilk normality test supports normality
-ks.test(r.t_facebook_1,"pnorm",mean=mean(r.t_facebook_1),sd=sd(r.t_facebook_1))                          # KS test supports normality
+shapiro.test(r.t_facebook_1)                                                                        # Shapiro-Wilk normality test supports normality
+ks.test(r.t_facebook_1,"pnorm",mean=mean(r.t_facebook_1),sd=sd(r.t_facebook_1))                     # KS test supports normality
 
 bst.models[nrow(bst.models)+1,] <- c("Facebook", 1, 0, fit.facebook_1.bst$d, 0)                     # adding to the table
 
 
+
+### chevron_1 ARFIMA model
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(v.t_chevron_1,ylim=c(0,5),                                                             
+        xlab="Year",ylab="GK volatility",main="Chevron Volatility 1/01/2016-12/31/2017")
+acf(v.t_chevron_1,lag.max=100,ylim=c(-0.2,1),main="")                                              # definitely appears to be long-memory
+pacf(v.t_chevron_1,lag.max=100,ylim=c(-0.2,1),main="")                                        
+
+fit.chevron_1.0d0 <- fracdiff(v.t_chevron_1-mean(v.t_chevron_1),nar=0,nma=0,M=50)                  # d term significant  
+summary(fit.chevron_1.0d0)
+
+fit.chevron_1.1d0 <- fracdiff(v.t_chevron_1-mean(v.t_chevron_1),nar=1,nma=0,M=50)                  # ar significant
+summary(fit.chevron_1.1d0)
+
+fit.chevron_1.2d0 <- fracdiff(v.t_chevron_1-mean(v.t_chevron_1),nar=2,nma=0,M=50)                  # all terms significant
+summary(fit.chevron_1.2d0)
+
+fit.chevron_1.0d1 <- fracdiff(v.t_chevron_1-mean(v.t_chevron_1),nar=0,nma=1,M=50)                  # ma sig
+summary(fit.chevron_1.0d1)
+
+fit.chevron_1.0d2 <- fracdiff(v.t_chevron_1-mean(v.t_chevron_1),nar=0,nma=2,M=50)                  # warning when computing correlation
+summary(fit.chevron_1.0d2)
+
+fit.chevron_1.1d1 <- fracdiff(v.t_chevron_1-mean(v.t_chevron_1),nar=1,nma=1,M=50)                  # warning   
+summary(fit.chevron_1.1d1)
+
+fit.chevron_1.1d2 <- fracdiff(v.t_chevron_1-mean(v.t_chevron_1),nar=1,nma=2,M=50)                  # warning when computing correlation        
+summary(fit.chevron_1.1d2)
+
+fit.chevron_1.2d1 <- fracdiff(v.t_chevron_1-mean(v.t_chevron_1),nar=2,nma=1,M=50)                  # warning when computing corr         
+summary(fit.chevron_1.2d1)
+
+
+
+c(fracdiff.AICC(fit.chevron_1.0d0),fracdiff.AIC(fit.chevron_1.0d0),fracdiff.BIC(fit.chevron_1.0d0))  
+c(fracdiff.AICC(fit.chevron_1.1d0),fracdiff.AIC(fit.chevron_1.1d0),fracdiff.BIC(fit.chevron_1.1d0))
+c(fracdiff.AICC(fit.chevron_1.2d0),fracdiff.AIC(fit.chevron_1.2d0),fracdiff.BIC(fit.chevron_1.2d0))
+c(fracdiff.AICC(fit.chevron_1.0d1),fracdiff.AIC(fit.chevron_1.0d1),fracdiff.BIC(fit.chevron_1.0d1))
+
+
+### chevron_1 model diagnostics: autocorrelation in residuals
+fit.chevron_1.bst <- fit.chevron_1.0d0                                                         
+
+r.t_chevron_1 <- fit.chevron_1.bst$residuals
+summary(r.t_chevron_1)                                                                         
+
+
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(r.t_chevron_1,ylim=c(-2,5),
+        xlab="Year",ylab="GK volatility",main="Chevron Volatility Residuals 1/01/2016-12/31/2017")
+abline(h=0,col="blue",lty=2)
+acf(r.t_chevron_1,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(r.t_chevron_1,lag.max=100,ylim=c(-0.2,1),main="")
+
+### chevron_1 residual normality check
+dev.new(height=6,width=12)
+par(mfrow=c(1,2),mex=0.75)
+hist(r.t_chevron_1,                                                                                # [Q] some outliers?
+     breaks=seq(-2,5,0.25),
+     freq=FALSE,
+     col="grey85",ylim=c(0,3),
+     main="Residual Histogram")                                                              
+z <- seq(-60,60,length=1000)                                      
+lines(z,dnorm(z,mean=mean(r.t_chevron_1),sd=sd(r.t_chevron_1)),lty=1,col="red")               
+qqnorm(r.t_chevron_1)                                                                         
+qqline(r.t_chevron_1)
+
+shapiro.test(r.t_chevron_1)                                                                        # Shapiro-Wilk normality test supports normality
+ks.test(r.t_chevron_1,"pnorm",mean=mean(r.t_chevron_1),sd=sd(r.t_chevron_1))                     # KS test supports normality
+
+bst.models[nrow(bst.models)+1,] <- c("Chevron", 1, 0, fit.chevron_1.bst$d, 0)                     # adding to the table
 
 
 
