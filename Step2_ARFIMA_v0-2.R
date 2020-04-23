bef0839c1490bc91b37a857b48263759bce7d294
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-23 14:53:18 -0600

    homedep1

--- Step2_ARFIMA_v0-2.R
+++ Step2_ARFIMA_v0-2.R
@@ -1894,6 +1894,79 @@ ks.test(r.t_att_1,"pnorm",mean=mean(r.t_att_1),sd=sd(r.t_att_1))                
 
 bst.models[nrow(bst.models)+1,] <- c("ATT", 1, 0, fit.att_1.bst$d, 0)     
 
+
+### homedep_1 ARFIMA model
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(v.t_homedep_1,ylim=c(0,4),                                                             
+        xlab="Year",ylab="GK volatility",main="Home Depot Volatility 1/01/2016-12/31/2017")
+acf(v.t_homedep_1,lag.max=100,ylim=c(-0.2,1),main="")                                                  
+pacf(v.t_homedep_1,lag.max=100,ylim=c(-0.2,1),main="")                                        
+
+fit.homedep_1.0d0 <- fracdiff(v.t_homedep_1-mean(v.t_homedep_1),nar=0,nma=0,M=50)                        # d term significant  
+summary(fit.homedep_1.0d0)
+
+fit.homedep_1.1d0 <- fracdiff(v.t_homedep_1-mean(v.t_homedep_1),nar=1,nma=0,M=50)                        # ar term not sig
+summary(fit.homedep_1.1d0)
+
+fit.homedep_1.2d0 <- fracdiff(v.t_homedep_1-mean(v.t_homedep_1),nar=2,nma=0,M=50)                        # ar terms not sig
+summary(fit.homedep_1.2d0)
+
+fit.homedep_1.0d1 <- fracdiff(v.t_homedep_1-mean(v.t_homedep_1),nar=0,nma=1,M=50)                        # ma not sig
+summary(fit.homedep_1.0d1)
+
+fit.homedep_1.0d2 <- fracdiff(v.t_homedep_1-mean(v.t_homedep_1),nar=0,nma=2,M=50)                        # ma terms not sig
+summary(fit.homedep_1.0d2)
+
+fit.homedep_1.1d1 <- fracdiff(v.t_homedep_1-mean(v.t_homedep_1),nar=1,nma=1,M=50)                        # all terms sig
+summary(fit.homedep_1.1d1)
+
+fit.homedep_1.1d2 <- fracdiff(v.t_homedep_1-mean(v.t_homedep_1),nar=1,nma=2,M=50)                        # warning
+summary(fit.homedep_1.1d2)
+
+fit.homedep_1.2d1 <- fracdiff(v.t_homedep_1-mean(v.t_homedep_1),nar=2,nma=1,M=50)                        # ar2 not sig  
+summary(fit.homedep_1.2d1)
+
+
+
+c(fracdiff.AICC(fit.homedep_1.0d0),fracdiff.AIC(fit.homedep_1.0d0),fracdiff.BIC(fit.homedep_1.0d0))
+c(fracdiff.AICC(fit.homedep_1.1d1),fracdiff.AIC(fit.homedep_1.1d1),fracdiff.BIC(fit.homedep_1.1d1)) 
+
+
+### homedep_1 model diagnostics: autocorrelation in residuals
+fit.homedep_1.bst <- fit.homedep_1.0d0                                                         
+
+r.t_homedep_1 <- fit.homedep_1.bst$residuals
+summary(r.t_homedep_1)                                                                         
+
+
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(r.t_homedep_1,ylim=c(-2,2),
+        xlab="Year",ylab="GK volatility",main="Home Depot Volatility Residuals 1/01/2016-12/31/2017")
+abline(h=0,col="blue",lty=2)
+acf(r.t_homedep_1,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(r.t_homedep_1,lag.max=100,ylim=c(-0.2,1),main="")
+
+### homedep_1 residual normality check
+dev.new(height=6,width=12)
+par(mfrow=c(1,2),mex=0.75)
+hist(r.t_homedep_1,                                                                                   
+     breaks=seq(-3,3,0.25),
+     freq=FALSE,
+     col="grey85",ylim=c(0,1.5),
+     main="Residual Histogram")                                                              
+z <- seq(-60,60,length=1000)                                      
+lines(z,dnorm(z,mean=mean(r.t_homedep_1),sd=sd(r.t_homedep_1)),lty=1,col="red")               
+qqnorm(r.t_homedep_1)                                                                         
+qqline(r.t_homedep_1)
+
+shapiro.test(r.t_homedep_1)                                                                      # Shapiro-Wilk normality test supports normality
+ks.test(r.t_homedep_1,"pnorm",mean=mean(r.t_homedep_1),sd=sd(r.t_homedep_1))                     # KS test supports normality
+
+bst.models[nrow(bst.models)+1,] <- c("Home Depot", 1, 0, fit.homedep_1.bst$d, 0)     
+
+
 ### MODEL FITTING PART TWO DATA ---------------------------------------------------------------------
 
 ### microsoft_2 ARFIMA model
