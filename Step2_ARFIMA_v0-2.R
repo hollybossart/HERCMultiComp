3350fe08c7a8323c2630d73ab74870448585f20c
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-23 11:10:55 -0600

    wellsfargo 1 added

--- Step2_ARFIMA_v0-2.R
+++ Step2_ARFIMA_v0-2.R
@@ -1300,6 +1300,84 @@ ks.test(r.t_disney_1,"pnorm",mean=mean(r.t_disney_1),sd=sd(r.t_disney_1))       
 
 bst.models[nrow(bst.models)+1,] <- c("Disney", 1, 0, fit.disney_1.bst$d, 0)                            # adding to the table
 
+
+
+### wellsfargo_1 ARFIMA model
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(v.t_wellsfargo_1,ylim=c(0,2),                                                             
+        xlab="Year",ylab="GK volatility",main="Wells Fargo Volatility 1/01/2016-12/31/2017")
+acf(v.t_wellsfargo_1,lag.max=100,ylim=c(-0.2,1),main="")                                                  # definitely appears to be long-memory
+pacf(v.t_wellsfargo_1,lag.max=100,ylim=c(-0.2,1),main="")                                        
+
+fit.wellsfargo_1.0d0 <- fracdiff(v.t_wellsfargo_1-mean(v.t_wellsfargo_1),nar=0,nma=0,M=50)                # d term significant  
+summary(fit.wellsfargo_1.0d0)
+
+fit.wellsfargo_1.1d0 <- fracdiff(v.t_wellsfargo_1-mean(v.t_wellsfargo_1),nar=1,nma=0,M=50)                # ar term not significant
+summary(fit.wellsfargo_1.1d0)
+
+fit.wellsfargo_1.2d0 <- fracdiff(v.t_wellsfargo_1-mean(v.t_wellsfargo_1),nar=2,nma=0,M=50)                # ar2 not sig
+summary(fit.wellsfargo_1.2d0)
+
+fit.wellsfargo_1.0d1 <- fracdiff(v.t_wellsfargo_1-mean(v.t_wellsfargo_1),nar=0,nma=1,M=50)                # ma sig
+summary(fit.wellsfargo_1.0d1)
+
+fit.wellsfargo_1.0d2 <- fracdiff(v.t_wellsfargo_1-mean(v.t_wellsfargo_1),nar=0,nma=2,M=50)                # ma2 not sig
+summary(fit.wellsfargo_1.0d2)
+
+fit.wellsfargo_1.1d1 <- fracdiff(v.t_wellsfargo_1-mean(v.t_wellsfargo_1),nar=1,nma=1,M=50)                # warning
+summary(fit.wellsfargo_1.1d1)
+
+fit.wellsfargo_1.1d2 <- fracdiff(v.t_wellsfargo_1-mean(v.t_wellsfargo_1),nar=1,nma=2,M=50)                 # warning
+summary(fit.wellsfargo_1.1d2)
+
+fit.wellsfargo_1.2d1 <- fracdiff(v.t_wellsfargo_1-mean(v.t_wellsfargo_1),nar=2,nma=1,M=50)                # ar 2 not sig        
+summary(fit.wellsfargo_1.2d1)
+
+
+
+c(fracdiff.AICC(fit.wellsfargo_1.0d0),fracdiff.AIC(fit.wellsfargo_1.0d0),fracdiff.BIC(fit.wellsfargo_1.0d0))
+c(fracdiff.AICC(fit.wellsfargo_1.1d0),fracdiff.AIC(fit.wellsfargo_1.1d0),fracdiff.BIC(fit.wellsfargo_1.1d0)) 
+c(fracdiff.AICC(fit.wellsfargo_1.0d1),fracdiff.AIC(fit.wellsfargo_1.0d1),fracdiff.BIC(fit.wellsfargo_1.0d1))
+
+
+
+### wellsfargo_1 model diagnostics: autocorrelation in residuals
+fit.wellsfargo_1.bst <- fit.wellsfargo_1.0d0                                                         
+
+r.t_wellsfargo_1 <- fit.wellsfargo_1.bst$residuals
+summary(r.t_wellsfargo_1)                                                                         
+
+
+dev.new(width=12,height=6)
+par(mfrow=c(3,1),mex=0.75)
+plot.ts(r.t_wellsfargo_1,ylim=c(-1,1.5),
+        xlab="Year",ylab="GK volatility",main="Wells Fargo Volatility Residuals 1/01/2016-12/31/2017")
+abline(h=0,col="blue",lty=2)
+acf(r.t_wellsfargo_1,lag.max=100,ylim=c(-0.2,1),main="")
+pacf(r.t_wellsfargo_1,lag.max=100,ylim=c(-0.2,1),main="")
+
+### wellsfargo_1 residual normality check
+dev.new(height=6,width=12)
+par(mfrow=c(1,2),mex=0.75)
+hist(r.t_wellsfargo_1,                                                                                   
+     breaks=seq(-2,2,0.25),
+     freq=FALSE,
+     col="grey85",ylim=c(0,3),
+     main="Residual Histogram")                                                              
+z <- seq(-60,60,length=1000)                                      
+lines(z,dnorm(z,mean=mean(r.t_wellsfargo_1),sd=sd(r.t_wellsfargo_1)),lty=1,col="red")               
+qqnorm(r.t_wellsfargo_1)                                                                         
+qqline(r.t_wellsfargo_1)
+
+shapiro.test(r.t_wellsfargo_1)                                                                                # Shapiro-Wilk normality test supports normality
+ks.test(r.t_wellsfargo_1,"pnorm",mean=mean(r.t_wellsfargo_1),sd=sd(r.t_wellsfargo_1))                         # KS test supports normality
+
+bst.models[nrow(bst.models)+1,] <- c("Wells Fargo", 1, 0, fit.wellsfargo_1.bst$d, 0)     
+
+
+
+
 ### MODEL FITTING PART TWO DATA ---------------------------------------------------------------------
 
 ### microsoft_2 ARFIMA model
