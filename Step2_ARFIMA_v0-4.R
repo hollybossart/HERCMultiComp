b5e52485d3efa89e3224b378e66f84ff82d56d57
Holly Bossart <hollybossart@u.boisestate.edu>
2020-04-29 14:34:37 -0600

    Facebook 2 added

--- Step2_ARFIMA_v0-4.R
+++ Step2_ARFIMA_v0-4.R
@@ -2946,8 +2946,8 @@ bst.models[nrow(bst.models)+1,] <- c("IBM", 2, 0, fit.ibm_2.bst$d, 0)           
 
 ### facebook_2 ARFIMA model
 dev.new(width=12,height=6)
-par(mfrow=c(3,1),mex=0.75)
-plot.ts(v.t_facebook_2,ylim=c(0,5),                                                             
+par(mfrow=c(3,1),mex=0.75) 
+plot.ts(v.t_facebook_2,ylim=c(0,9),                                                                   # take notice of the range                                                            
         xlab="Year",ylab="GK volatility",main="Facebook Volatility 2/01/2018-12/31/2019")
 acf(v.t_facebook_2,lag.max=100,ylim=c(-0.2,1),main="")                                         
 pacf(v.t_facebook_2,lag.max=100,ylim=c(-0.2,1),main="")                                        
@@ -2955,19 +2955,19 @@ pacf(v.t_facebook_2,lag.max=100,ylim=c(-0.2,1),main="")
 fit.facebook_2.0d0 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=0,nma=0,M=50)                  # d term significant  
 summary(fit.facebook_2.0d0)
 
-fit.facebook_2.1d0 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=1,nma=0,M=50)                  # ar significant
+fit.facebook_2.1d0 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=1,nma=0,M=50)                  # ar not significant
 summary(fit.facebook_2.1d0)
 
-fit.facebook_2.2d0 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=2,nma=0,M=50)                  # ar2 not sig
+fit.facebook_2.2d0 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=2,nma=0,M=50)                  # ar2 sig
 summary(fit.facebook_2.2d0)
 
-fit.facebook_2.0d1 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=0,nma=1,M=50)                  # ma sig
+fit.facebook_2.0d1 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=0,nma=1,M=50)                  # ma not sig
 summary(fit.facebook_2.0d1)
 
 fit.facebook_2.0d2 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=0,nma=2,M=50)                  # ma1 sig
 summary(fit.facebook_2.0d2)
 
-fit.facebook_2.1d1 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=1,nma=1,M=50)                  # all sig     
+fit.facebook_2.1d1 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=1,nma=1,M=50)                  # cannot compute correlation    
 summary(fit.facebook_2.1d1)
 
 fit.facebook_2.1d2 <- fracdiff(v.t_facebook_2-mean(v.t_facebook_2),nar=1,nma=2,M=50)                  # warning when computing correlation        
@@ -2995,7 +2995,7 @@ summary(r.t_facebook_2)
 
 dev.new(width=12,height=6)
 par(mfrow=c(3,1),mex=0.75)
-plot.ts(r.t_facebook_2,ylim=c(-2,5),
+plot.ts(r.t_facebook_2,ylim=c(-6, 6),
         xlab="Year",ylab="GK volatility",main="Facebook Volatility Residuals 2/01/2018-12/31/2019")
 abline(h=0,col="blue",lty=2)
 acf(r.t_facebook_2,lag.max=100,ylim=c(-0.2,1),main="")
@@ -3005,9 +3005,9 @@ pacf(r.t_facebook_2,lag.max=100,ylim=c(-0.2,1),main="")
 dev.new(height=6,width=12)
 par(mfrow=c(1,2),mex=0.75)
 hist(r.t_facebook_2,                                                                                # [Q] some outliers?
-     breaks=seq(-2,5,0.25),
+     breaks=seq(-6,6,0.25),
      freq=FALSE,
-     col="grey85",ylim=c(0,3),
+     col="grey85",ylim=c(0,0.75),
      main="Residual Histogram")                                                              
 z <- seq(-60,60,length=1000)                                      
 lines(z,dnorm(z,mean=mean(r.t_facebook_2),sd=sd(r.t_facebook_2)),lty=1,col="red")               
